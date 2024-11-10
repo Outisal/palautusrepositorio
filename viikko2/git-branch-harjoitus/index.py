8d82363102b0c6649988fcabce2bb1d77c57aab0
Outi Salonen <147878561+Outisal@users.noreply.github.com>
2024-11-10 20:44:46 +0200

    Edit index py in bugikorjaus

--- viikko2/git-branch-harjoitus/index.py
+++ viikko2/git-branch-harjoitus/index.py
@@ -4,12 +4,12 @@ from logger import logger
 from summa import summa
 from erotus import erotus
 
-logger("aloitetaan")
+logger("aloitetaan ohjelma")
 
 x = int(input("luku1: "))
 y = int(input("luku2: "))
-print(f"{summa(x, y)}")
-print(f"{erotus(x, y)}")
+print(f"Lukujen {x} ja {y} summa on {summa(x, y)}") # muutos bugikorjaus-branch
+print(f"Lukujen {x} ja {y} erotus on {erotus(x, y)}") # muutos bugikorjaus-branch
 
 logger("lopetetaan ohjelma")
-print("goodbye!") #lisäys bugikorjaus-branchissa
+print("goodbye!")
