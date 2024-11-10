6fe94709c8caee3e0259dd76ed4d7a2f80a0a5a5
Outi Salonen <147878561+Outisal@users.noreply.github.com>
2024-11-10 20:29:57 +0200

    Add comment to index.py

--- viikko2/git-branch-harjoitus/index.py
+++ viikko2/git-branch-harjoitus/index.py
@@ -1,3 +1,5 @@
+#tehdään alussa importit
+
 from logger import logger
 from summa import summa
 from erotus import erotus
@@ -7,6 +9,6 @@ logger("aloitetaan")
 x = int(input("luku1: "))
 y = int(input("luku2: "))
 print(f"{summa(x, y)}")
-print(f"erotus(x, y)}")
+print(f"{erotus(x, y)}")
 
 logger("lopetetaan")
