6bdab30ddaddd54c7de7c07e3b1688f807a402ce
Outi Salonen <147878561+Outisal@users.noreply.github.com>
2024-11-11 15:00:50 +0200

    Add division to index in main

--- viikko2/git-branch-harjoitus/index.py
+++ viikko2/git-branch-harjoitus/index.py
@@ -4,6 +4,7 @@ from logger import logger
 from summa import summa
 from erotus import erotus
 from tulo import tulo
+from osamaara import osamaara
 
 logger("aloitetaan ohjelma") #muutos mainissa
 
@@ -11,7 +12,8 @@ x = int(input("luku1: "))
 y = int(input("luku2: "))
 print(f"{x} + {y} = {summa(x, y)}") # muutos mainissa
 print(f"{x} - {y} = {erotus(x, y)}") # muutos mainissa
-print(f"{x} * {y} ={tulo(x, y}")
+print(f"{x} * {y} = {tulo(x, y)}")
+print(f"{x} / {y} = {osamaara(x, y)}")
 
 logger("lopetetaan ohjelma")
 print("goodbye!") #muutos bugikorjaus
