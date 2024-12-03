9dd0aef2baf990107885a07b685391b4c1e4b94c
Outi Salonen <147878561+Outisal@users.noreply.github.com>
2024-12-03 20:02:30 +0200

    wk6_2 query language NHL2 method Or

--- viikko6/query-language/src/index.py
+++ viikko6/query-language/src/index.py
@@ -1,6 +1,6 @@
 from player_statistics import Statistics
 from player_reader import PlayerReader
-from matchers import Not, HasFewerThan, All, And, HasAtLeast, PlaysIn
+from matchers import Not, HasFewerThan, All, And, HasAtLeast, PlaysIn, Or
 
 def main():
     url = "https://studies.cs.helsinki.fi/nhlstats/2023-24/players.txt"
@@ -8,28 +8,16 @@ def main():
     stats = Statistics(reader)
 
     matcher = And(
-        HasAtLeast(5, "goals"),
-        HasAtLeast(20, "assists"),
-        PlaysIn("PHI")
+    HasAtLeast(70, "points"),
+    Or(
+        PlaysIn("NYR"),
+        PlaysIn("FLA"),
+        PlaysIn("BOS")
+    )
     )
 
-    matcher1 = And(
-    Not(HasAtLeast(2, "goals")),
-    PlaysIn("NYR"))
-
-    matcher2 = And(
-    HasFewerThan(2, "goals"),
-    PlaysIn("NYR")
-    )
-
-
-    for player in stats.matches(matcher1):
+    for player in stats.matches(matcher):
         print(player)
-    for player in stats.matches(matcher2):
-        print(player)
-    
-    filtered_with_all = stats.matches(All())
-    print(len(filtered_with_all))
 
 if __name__ == "__main__":
     main()
