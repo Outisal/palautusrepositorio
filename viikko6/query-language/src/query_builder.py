5b3ce4d8c2d7510215099fe8108366f1cf01f7f0
Outi Salonen <147878561+Outisal@users.noreply.github.com>
2024-12-03 20:54:31 +0200

    Wk6_5 Improve query language2

--- viikko6/query-language/src/query_builder.py
+++ viikko6/query-language/src/query_builder.py
@@ -1,4 +1,4 @@
-from matchers import PlaysIn, HasAtLeast, HasFewerThan, And, All
+from matchers import PlaysIn, HasAtLeast, HasFewerThan, And, All, Or
 
 class QueryBuilder:
     def __init__(self):
@@ -15,8 +15,18 @@ class QueryBuilder:
     def has_fewer_than(self, value, attr):
         self._matchers.append(HasFewerThan(value, attr))
         return self
+    
+    def one_of(self, *matchers):
+        self._matchers = [Or(*matchers)]
+        return self
 
     def build(self):
         if not self._matchers:
-            return All()
-        return And(*self._matchers)
+            result = All()
+        elif len(self._matchers) == 1:
+            result = self._matchers[0]
+        else:
+            result = And(*self._matchers)
+
+        self._matchers = []
+        return result
